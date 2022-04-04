fcf6e7b56e9e8501e595bfb453a8d4d5501d137a
Bindwin <bindwin.dev@gmail.com>
2022-04-04 03:59:31 +0200

    Add files via upload

---
+++ PMU_Placec.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Reddit Place - Armée de Kameto
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  On va récuperer ce qui nous est du de droit.
+// @author       Adcoss95
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://styles.redditmedia.com/t5_4eiiz1/styles/communityIcon_ojy24r8j90o81.jpg
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://github.com/Bindwin/PLACE/blob/cb6e60c6c5c7d4bb9abf517be7ac756126a8af3c/PMU.png";
+            i.style = "position: absolute;left: 9;top: 1250;image-rendering: pixelated;width: 64px;height: 64px;";
+            console.log(i);
+            return i;
+        })())
+ 
+    }, false);
+ 
+}
